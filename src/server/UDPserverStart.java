67a18e8593ea1092672036fb0357481c14de8bcb
Hannah <dvoryanina1903@icloud.com>
2024-11-20 12:48:36 +0400

    Minor Fixes
    Fixed some errors, Prepared for UDP protocol testing.

--- src/server/UDPserverStart.java
+++ src/server/UDPserverStart.java
@@ -1,16 +1,16 @@
-package server;
+package client;
 
+import com.google.common.net.HostAndPort;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramChannel;
-
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.handler.timeout.IdleStateHandler;
-import io.netty.util.concurrent.FutureListener;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import server.chananelHandler.ExceptionDuplexHandler;
@@ -18,18 +18,16 @@ import server.chananelHandler.inbound.CryptInitInHandler;
 import server.chananelHandler.inbound.DecodeSSHandler;
 import server.chananelHandler.inbound.UdpProxyInHandler;
 import server.chananelHandler.outbound.EncodeSSOutHandler;
-import server.config.ServerConfig;
 import server.menu.ServerHelper;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 
-public class ServerStart {
-
-    private static final Logger logger = LoggerFactory.getLogger(ServerStart.class);
+public class UDPserverStart {
+    private static final Logger logger = LoggerFactory.getLogger(UDPserverStart.class);
 
     private static server.config.ServerConfig ServerConfig;
-    private static final EventLoopGroup bossLoopGroup = new NioEventLoopGroup(server.config.ServerConfig.serverConfig.getBossThreadNumber());
-    private static final EventLoopGroup worksLoopGroup = new NioEventLoopGroup(server.config.ServerConfig.serverConfig.getWorkersThreadNumber());
+    private static final EventLoopGroup eventLoopGroup = new NioEventLoopGroup(server.config.ServerConfig.serverConfig.getBossThreadNumber());
 
     private static final Bootstrap serverBootstrap = new Bootstrap();
 
@@ -39,49 +37,29 @@ public class ServerStart {
     }
 
     private static void startupServer() throws InterruptedException {
-        serverBootstrap.group(bossLoopGroup, worksLoopGroup)
+        serverBootstrap.group(eventLoopGroup)
                 .channel(NioDatagramChannel.class)
-                .childHandler(new ChannelInitializer<DatagramChannel>() {
-            protected void initChannel(DatagramChannel ch) {
-                ch.pipeline()
-                        .addLast(new IdleStateHandler(1000, 1000, ServerConfig.serverConfig.getClientIdle(), TimeUnit.SECONDS))
-                        .addLast(new CryptInitInHandler()) // Assuming CryptInitInHandler works with UDP
-                        .addLast(new DecodeSSHandler()) // Assuming DecodeSSHandler works with UDP
-                        .addLast(new UdpProxyInHandler()) // New UDP handler
-                        .addLast(new EncodeSSOutHandler())
-                        .addLast(new ExceptionDuplexHandler());
-            }
-        });
+                .option(ChannelOption.SO_BROADCAST, true)
+                .handler(new ChannelInitializer<DatagramChannel>() {
+                    protected void initChannel(DatagramChannel ch) {
+                        ch.pipeline()
+                                .addLast(new IdleStateHandler(1000, 1000, ServerConfig.serverConfig.getClientIdle(), TimeUnit.SECONDS))
+                                .addLast(new CryptInitInHandler()) // Assuming CryptInitInHandler works with UDP
+                                .addLast(new DecodeSSHandler()) // Assuming DecodeSSHandler works with UDP
+                                .addLast(new UdpProxyInHandler()) // New UDP handler
+                                .addLast(new EncodeSSOutHandler())
+                                .addLast(new ExceptionDuplexHandler());
+                    }
+                });
+
         InetSocketAddress bindAddress = getAddress(ServerConfig.serverConfig.getLocalAddress());
         ChannelFuture channelFuture = serverBootstrap.bind(bindAddress).sync();
         logger.info("shadowsocks server [udp] running at {}", bindAddress);
         channelFuture.channel().closeFuture().sync();
-
     }
 
-
-    private static InetSocketAddress getAddress(String address) {
-        if (!address.contains(":")) {
-            throw new IllegalArgumentException("illegal address: " + address);
-        }
-        String host;
-        int port;
-        if (address.startsWith("[")) {
-            // IPv6 address
-            int closingBracketIndex = address.indexOf("]");
-            if (closingBracketIndex == -1) {
-                throw new IllegalArgumentException("illegal address: " + address);
-            }
-            host = address.substring(1, closingBracketIndex);
-            port = Integer.parseInt(address.substring(closingBracketIndex + 2));
-        } else {
-            // IPv4 address
-            host = address.substring(0, address.indexOf(":"));
-            port = Integer.parseInt(address.substring(address.indexOf(":") + 1));
-        }
-        return new InetSocketAddress(host, port);
+    public static InetSocketAddress getAddress(String address) {
+        HostAndPort hostAndPort = HostAndPort.fromString(address);
+        return new InetSocketAddress(hostAndPort.getHost(), hostAndPort.getPort());
     }
-
 }
-
-
